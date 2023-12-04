96dae49bb02db64193b239c22bcb643f446c4f37
sajib <sajibgoswmai@gmail.com>
2023-12-04 13:40:22 +0600

    test6

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-dotnet publish "BizWebAPI.csproj" -c Release -o /app/publish && cp app.db /app/publish && export ASPNETCORE_URLS=http://+:5003 
+dotnet publish "BizWebAPI.csproj" -c Release -o /output && cp app.db /output && export ASPNETCORE_URLS=http://+:5003 
